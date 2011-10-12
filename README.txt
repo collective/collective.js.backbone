4e0b4fcb427e0f19a5d469f42c241282adb7e8e5
href <href@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-10-12 09:26:49 +0000

    Initial commit
    git-svn-id: http://svn.plone.org/svn/collective/collective.js.underscore/trunk@245419 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ README.txt
@@ -0,0 +1,17 @@
+Introduction
+============
+
+ * Bundles underscore.js for use with Plone.
+ * The version number of this module is equal to the used underscore.js version.
+
+underscore.js
+=============
+
+(c) 2011 Jeremy Ashkenas, DocumentCloud Inc.
+
+Underscore is freely distributable under the MIT license.
+Portions of Underscore are inspired or borrowed from Prototype,
+Oliver Steele's Functional, and John Resig's Micro-Templating.
+
+For all details and documentation:
+http://documentcloud.github.com/underscore
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.2.0'
+
+setup(name='collective.js.underscore',
+      version=version,
+      description="underscore.js for plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+
+        ],
+      keywords='javascript plone underscore',
+      author='Denis Krienbuehl',
+      author_email='denis.krienbuehl@gmail.com',
+      url='http://svn.plone.org/svn/collective/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
